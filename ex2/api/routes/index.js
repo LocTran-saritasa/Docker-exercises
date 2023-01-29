1613292e80025fcfc7d3d29d4e6b088c0de3c654
LocTran-saritasa <loc.tran@saritasa.com>
2023-01-29 11:31:43 +0700

    Remove unuse api

--- ex2/api/routes/index.js
+++ ex2/api/routes/index.js
@@ -1,9 +1,7 @@
 import { authRoute } from './auth.js';
-import { userRoute } from './user.js';
 
 const route = app => {
     app.use('/auth', authRoute);
-    app.use('/user', userRoute);
 }
 
 export default route;
--- ex2/api/routes/user.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { userController } from '../controllers/userController.js';
-
-const router = express.Router();
-
-router.get('/all', userController.getAllUser)
-
-export const userRoute = router;
