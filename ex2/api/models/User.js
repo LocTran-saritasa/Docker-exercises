d6ac8d14d17dd572284edaa261c39a0c51dad959
LocTran <loc.tran@saritasa.com>
2023-02-01 13:39:40 +0700

    Remove unuse lib

--- ex2/api/models/User.js
+++ ex2/api/models/User.js
@@ -1,10 +1,11 @@
 import { client } from "../db/index.js";
 
-class UserModel {
+export class UserModel {
+    email;
+    password
+
     async getAll () {
         const usersQuery = await client.query('SELECT * FROM Users')
         return usersQuery.rows
     }
 }
-
-export const userModel = new UserModel()
